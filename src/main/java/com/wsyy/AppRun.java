8f620e1a6c2959815753815d0589dc40f54ce8e6
wwpw <32744395+wangpeiming@users.noreply.github.com>
2020-10-30 14:27:08 +0800

    Update AppRun.java

--- src/main/java/com/wsyy/AppRun.java
+++ src/main/java/com/wsyy/AppRun.java
@@ -2,15 +2,11 @@ package com.wsyy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 
 import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 
 @SpringBootApplication(exclude = DruidDataSourceAutoConfigure.class)
-@EnableScheduling
-@EnableTransactionManagement
 public class AppRun {
 	
 	public static void main(String[] args) {
